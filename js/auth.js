c6ba6b0cbe478ed4553f2d9c743713a7afa764a2
MOMEN186 <57963805+MOMEN186@users.noreply.github.com>
2025-04-03 20:46:19 +0200

    added jwt and logout

--- js/auth.js
+++ js/auth.js
@@ -1,13 +1,14 @@
-const port = 8080;//changr the port based on your pc
-
+const port = 5500;//change the port based on your pc
+const api_port=8080
 
 export async function signup(event) {
   event.preventDefault();
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
   const email = document.getElementById("email").value;
+  console.log(email)
   try {
-    const result = await fetch(`http://localhost:${port}/auth/signup`, {
+    const result = await fetch(`http://localhost:${api_port}/auth/signup`, {
       method: "POST",
       headers: {
         Accept: "*/*",
@@ -20,7 +21,10 @@ export async function signup(event) {
       }),
     });
     const data = await result.json();
-    console.log(data);
+    console.log(data.status);
+    if (result.ok) {
+      window.location.href=`http://localhost:${port}/html/login.html`
+    }
     return data;
   } catch (e) {
     console.log(e);
@@ -29,11 +33,11 @@ export async function signup(event) {
 
 export async function login(event) {
   event.preventDefault(); // Prevent pagesrefresh
-  const email = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   try {
-    const result = await fetch(`http://localhost:${port}/auth/login`, {
+    const result = await fetch(`http://localhost:${api_port}/auth/login`, {
       method: "POST",
       headers: {
         Accept: "*/*",
@@ -46,10 +50,12 @@ export async function login(event) {
     });
 
     const data = await result.json();
+    console.log(data);
     if (result.ok) {
       console.log("Login successful:", data);
-      localStorage.setItem("token", data.token); // Save token to localStorage
-      //window.location.href = "home.html"; // Redirect to homepage
+      localStorage.setItem("token", JSON.stringify(data.token)); // Save token to localStorage
+      console.log(data?.token)
+      window.location.href = `http://localhost:${port}/html/home.html`
     } else {
       console.error("Login failed:", data.message);
       alert("Login failed: " + data.message); // Show error message to the user
@@ -62,21 +68,38 @@ export async function login(event) {
 
 export async function logout() {
   try {
-    const result = await fetch(`http://localhost:${port}/auth/logout`, {
+    const result = await fetch(`http://localhost:${api_port}/auth/logout`, {
       method: "DELETE",
       headers: {
         Accept: "*/*",
         "Content-Type": "application/json",
+        token: JSON.parse(localStorage.getItem(token))
       },
+      body: {
+        
+      }
     });
     const data = await result.json();
-    localStorage.removeItem("token");
+
+    if (result.ok) {
+      localStorage.removeItem("token");
+    }
+    else {
+      console.error("can't logout")
+    }
     return data;
   } catch (e) {
     console.log(e);
   }
 }
 
+function isAuthenticated() {
+  if (`http://localhost:${port}/html/home.html` === window.location.href) {
+    const token = JSON.parse(localStorage.getItem("token"));
+    if (!token) window.location.href = `http://127.0.0.1:${port}/html/login.html`;
+  }
+}
+document.addEventListener("DOMContentLoaded",isAuthenticated);
 document.getElementById("login-form")?.addEventListener("submit", login);
 document.getElementById("signup-form")?.addEventListener("submit", signup);
 document.getElementsByClassName("sign-out-btn")[0]?.addEventListener("click", logout);
